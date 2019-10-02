6cf7da40ddbdeb6ade9391f6d6afe9b73b93fb1c
Keyhan <keyhan.igo@gmail.com>
2019-10-02 16:48:54 +0300

    alternative method

--- pusherPlugin/index.js
+++ pusherPlugin/index.js
@@ -12,26 +12,26 @@ module.exports = {
         pusher.connect();
 
         const channel = new Channel(pusher.subscribe(channel_name));
-        const ref = new java.lang.ref.WeakReference(pusher);
-        const owner = ref.get();
 
-
-        /*if (owner.eventChannels.has(`${channel_name}_${event_name}`)) {
-            const eventData = owner.eventChannels.get(`${channel_name}_${event_name}`)
-            alert(eventData);
-        }*/
-
-        let sel = new com.pusher.client.channel.SubscriptionEventListener({
+        /*let sel = new com.pusher.client.channel.SubscriptionEventListener({
             onEvent: function(channel, event, data) {
                 console.log("Event", event, "received event with data: " + JSON.stringify(data));
             }
-        });
+        });*/
 
+        let EventListener;
+        function initializeEventListener() {
+            EventListener = java.lang.Object.extend({
+                interfaces: [com.pusher.client.channel.SubscriptionEventListener],
+                onEvent: function() {
+                    console.log("WHATEVER");
+                }
+            });
+        }
+        initializeEventListener();
 
-
-        //alert(binding);
-
-        //channel.bind('my-event', sel);
+        console.log(new EventListener());
+        //channel.bind(event_name, new EventListener());
 
 
     }
